229a937ff4f02f9caa40a1fa314806ed6d82efa1
Nikolay Repin <nklrpn@yandex.ru>
2016-11-16 01:18:42 +1000

    Delete default config

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
