eaa3988de736f76a2faad9f4a56742ac32813040
Nikolay Repin <nklrpn@yandex.ru>
2016-11-22 23:57:05 +1000

    Edit init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,8 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo gunicorn -c /etc/gunicorn.d/ask ask.wsgi:application
+
 sudo /etc/init.d/gunicorn restart
